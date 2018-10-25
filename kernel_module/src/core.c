fb173787fbbdbae4573357b11a7808b2d7300b55
Chirag Jain <csjain@ncsu.edu>
2018-10-25 16:31:56 -0400

    error fix

--- kernel_module/src/core.c
+++ kernel_module/src/core.c
@@ -50,11 +50,19 @@ extern struct miscdevice memory_container_dev;
 
 int memory_container_mmap(struct file *filp, struct vm_area_struct *vma)
 {
-        printk("Print from memory_container_mmap %d", LEN+2*PAGE_SIZE);
-        static int *kmalloc_area = NULL;
+        printk("PAGE_SIZE: %lu", PAGE_SIZE);
+        printk("Requested siz: %lu", vma->vm_end - vma->vm_start);
+
+
+        int *kmalloc_area = NULL;
+        int *kmalloc_ptr = NULL;
+        u64 vtp = virt_to_phys((void *)kmalloc_area);
+
+        printk("VTP: %llu", vtp);
+
         kmalloc_ptr=kmalloc(LEN+2*PAGE_SIZE, GFP_KERNEL);
         kmalloc_area=(int *)(((unsigned long)kmalloc_ptr + PAGE_SIZE -1) & PAGE_MASK);
-        vma->flags |= VM_LOCKED;
+
         if (remap_pfn_range(vma, vma->vm_start, virt_to_phys((void *)kmalloc_area) >> PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot) < 0)
         {
                 printk("remap_pfn_range failed\n");
