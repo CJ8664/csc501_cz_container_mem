a302e326dc161c819bbfb5afaa33b3cbdbef5e81
Chirag Jain <csjain@ncsu.edu>
2018-10-28 20:26:20 -0400

    Update benchmark

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -135,13 +135,13 @@ int main(int argc, char *argv[])
         }
 
         // try delete something
-        // i = rand() % number_of_objects;
-        // i = 1;
-        // mcontainer_lock(devfd, i);
-        // gettimeofday(&current_time, NULL);
-        // mcontainer_free(devfd, i);
-        // fprintf(fp, "D\t%d\t%d\t%ld\t%d\t%d\t%s\n", getpid(), cid, current_time.tv_sec * 1000000 + current_time.tv_usec, i, max_size_of_objects, "delete_an_object");
-        // mcontainer_unlock(devfd, i);
+        i = rand() % number_of_objects;
+        i = 1;
+        mcontainer_lock(devfd, i);
+        gettimeofday(&current_time, NULL);
+        mcontainer_free(devfd, i);
+        fprintf(fp, "D\t%d\t%d\t%ld\t%d\t%d\t%s\n", getpid(), cid, current_time.tv_sec * 1000000 + current_time.tv_usec, i, max_size_of_objects, "delete_an_object");
+        mcontainer_unlock(devfd, i);
 
 
         // done with works, cleanup and wait for other processes.
